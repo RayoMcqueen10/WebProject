b472dc1827e4bce4772f58c108619847c6aecd85
RayoMcqueen10 <pedro.cernas1877@alumnos.udg.mx>
2024-09-10 10:50:59 -0600

    p5 signup

--- gamezone.py
+++ gamezone.py
@@ -6,5 +6,9 @@ gamezone = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@gamezone.route('/signup)')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':  
    gamezone.run(debug=True,port=3300)  
